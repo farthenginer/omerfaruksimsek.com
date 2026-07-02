4fe45c796400d8514398f19f1fe8f6643a84cb28
omersimsek <149330923+farthenginer@users.noreply.github.com>
2026-07-02 13:44:26 +0300

    site adi, aciklama ve favicon guncellendi

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ömer Faruk Şimşek';
+export const SITE_DESCRIPTION = 'Personal website, blog and portfolio of Ömer Faruk Şimşek.';
